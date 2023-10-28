6d5f55c8b6b188c5b0bc6bfe7a29a9998a40c7ff
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2023-10-28 23:45:37 +0530

    Adding Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,147 @@
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Room,
+  Twitter,
+} from "@mui/icons-material";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  display: flex;
+  ${mobile({ flexDirection: "column" })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ display: "none" })}
+`;
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ backgroundColor: "#faf6f6" })}
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>SANDEEP.</Logo>
+        <Desc>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don’t look even slightly believable.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <YouTubeIcon />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Usefull Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Get in touch with us</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} /> Indiqube Gamma,Outer Ring
+          Road,Kadubeesanahalli, Marathahalli 560103
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +91 8112912494
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} />{" "}
+          sandeepstructure@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
